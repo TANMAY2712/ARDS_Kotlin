2fb7551b718bcd12f8bda200a9efcdfa169b522e
Tanmay <tanmay2712d@gmail.com>
2025-03-05 16:59:48 +0530

    some more design added

--- ARDS/app/src/main/java/com/ards/ui/history/HistoryFragment.kt
+++ ARDS/app/src/main/java/com/ards/ui/history/HistoryFragment.kt
@@ -4,19 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.ards.databinding.FragmentNotificationsBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ards.databinding.FragmentHistoryBinding
+import com.ards.ui.history.adapter.HistoryAdapter
+import com.ards.ui.history.model.Recent
 
 class HistoryFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentHistoryBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
+    private lateinit var historyAdapter: HistoryAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,13 +27,21 @@ class HistoryFragment : Fragment() {
         val notificationsViewModel =
             ViewModelProvider(this).get(HistoryViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        binding.rvTrainHistory.layoutManager = LinearLayoutManager(requireContext())
+
+        val trainList = listOf(
+            Recent("Sabarmati Express", "12578", "DELHI", "11:00", "BHOPAL", "23:35", 2),
+            Recent("Ernakulam Express", "12911", "DELHI", "17:50", "ERNAKULAM", "22:15", 1),
+            Recent("Rajdhani Express", "12578", "DELHI", "17:00", "BHUBANESHWAR", "21:35", 2),
+            Recent("Kranti Express", "12911", "DELHI", "17:50", "KAHIPUR", "22:15", 1),
+            Recent("Avadh Express", "12578", "DELHI", "11:00", "BAREILLY", "23:35", 1)
+        )
+
+        historyAdapter = HistoryAdapter(trainList)
+        binding.rvTrainHistory.adapter = historyAdapter
         return root
     }
 
--- ARDS/app/src/main/java/com/ards/ui/home/HomeFragment.kt
+++ ARDS/app/src/main/java/com/ards/ui/home/HomeFragment.kt
@@ -7,7 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ards.databinding.FragmentHomeBinding
+import com.ards.ui.history.adapter.HistoryAdapter
+import com.ards.ui.history.adapter.RecentAdapter
+import com.ards.ui.history.model.Recent
 
 class HomeFragment : Fragment() {
 
@@ -16,7 +20,7 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
+    private lateinit var recentAdapter: RecentAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -27,6 +31,19 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        binding.recentTrainsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        val trainList = listOf(
+            Recent("Sabarmati Express", "12578", "DELHI", "11:00", "BHOPAL", "23:35", 2),
+            Recent("Ernakulam Express", "12911", "DELHI", "17:50", "ERNAKULAM", "22:15", 1),
+            Recent("Rajdhani Express", "12578", "DELHI", "17:00", "BHUBANESHWAR", "21:35", 2),
+            Recent("Kranti Express", "12911", "DELHI", "17:50", "KAHIPUR", "22:15", 1),
+            Recent("Avadh Express", "12578", "DELHI", "11:00", "BAREILLY", "23:35", 1)
+        )
+
+        recentAdapter = RecentAdapter(trainList)
+        binding.recentTrainsRecyclerView.adapter = recentAdapter
 
         return root
     }
